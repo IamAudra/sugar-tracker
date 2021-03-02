559154965b6b4ffc0c789d0d3d28315314e69a49
Audra <73372336+IamAudra@users.noreply.github.com>
2021-03-02 15:38:42 -0500

    Rewrote and reformatted back-end pages.

--- src/Pages/DataResults/RecipePages.js
+++ src/Pages/DataResults/RecipePages.js
@@ -1,16 +1,47 @@
-import  {Footer, NavBar} from "../../Components";
+import {Footer, NavBar} from "../../Components";
 
 export const RecipePages = () => (
+
     <>
         <NavBar/>
-        <div className="p-10 bg-blue-700">
-            Links for Diabetic Friendly Recipes
-            Favorite Option?
-            Connect to Sugar Tracker result?
-            Connect to Fitbit Food result?
+        <div className="p-10 bg-blue-200  text-purple-700">
+            <p className="text-6xl  flex justify-center">
+                Recipe Links
+            </p>
+        <div className=" ">
+            <p className="text-2xl">
+
+            </p>
+                <p className="text-2xl py-6">
+                <a href="https://diabetes.org/healthy-living/recipes-nutrition"> American Diabetic Association Recipes</a>
+            </p>
+
+            <p className="text-2xl py-3">
+                Diabetic Desserts
+                </p>
+                <p className="text-2xl py-3">
+
+                  <a href="https://www.tasteofhome.com/collection/diabetic-desserts/">95 Diabetic Desserts: Taste of Home</a>
+            </p>
+
+            <p className="text-2xl py-3">
+               <a href="https://www.soulfoodandsoutherncooking.com/diabetic-soul-food-recipes.html">Soul Food and Southern Cooking</a>
+            </p>
+            <p className="text-2xl py-3">
+                "These diabetic soul food recipes are for you If you're living with diabetes, have a family history of diabetes or have just been diagnosed with diabetes."
+            </p>
+
+            <p className="text-2xl py-3">
+                <a href="https://blackdoctor.org/category/healthy-living/healthy-recipes-for-diabetics/">Black Doctor.org</a>
+            </p>
+            <p className="text-2xl py-3">
+             "Where Wellness & Culture Connect: Healthy Recipes for Diabetics"
+            </p>
+            <p className="text-2xl py-3">
+                <a href="https://www.tasteofhome.com/collection/diabetic-vegetarian-recipes/">A Taste of Home: 70 Diabetic-Friendly Vegetarian Recipes</a>
+            </p>
         </div>
-        <Footer/>
-
-    </>
-
-)
+        </div>
+       <Footer/>
+        </>
+    )
--- src/Pages/DataResults/index.js
+++ src/Pages/DataResults/index.js
@@ -2,4 +2,8 @@ export {FitBit} from './selections/FitBit'
 export {MedicationResults} from './MedicationResults'
 export  {RecipePages} from './RecipePages'
 export  {SugarTracker} from './SugarTracker'
+export {CommentPage} from './CommentPage'
+export {LoginPage} from './LoginPage'
+
+
 

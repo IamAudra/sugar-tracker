2ed6e0ee0c52a29922356d4a7693cb88ce102de3
Audra <73372336+IamAudra@users.noreply.github.com>
2021-03-09 20:16:25 -0500

    Created Data folder and inserted FitBit.sql and users.sql.

---
+++ src/Data/users.sql
@@ -0,0 +1,10 @@
+insert into MOCK_DATA (id, fullname, email, password) values (1, 'Grier', 'glacrouts0@de.vu', 'Impala');
+insert into MOCK_DATA (id, fullname, email, password) values (2, 'Ashli', 'askipper1@blinklist.com', 'Southern sea lion');
+insert into MOCK_DATA (id, fullname, email, password) values (3, 'Silvana', 'shenriquet2@globo.com', 'American bison');
+insert into MOCK_DATA (id, fullname, email, password) values (4, 'Cyndi', 'cjakovijevic3@paginegialle.it', 'Duiker, gray');
+insert into MOCK_DATA (id, fullname, email, password) values (5, 'Saree', 'sickovic4@youtube.com', 'Common waterbuck');
+insert into MOCK_DATA (id, fullname, email, password) values (6, 'Sheilakathryn', 'sgummie5@unblog.fr', 'African bush squirrel');
+insert into MOCK_DATA (id, fullname, email, password) values (7, 'Fairfax', 'fgresser6@sakura.ne.jp', 'Grey-footed squirrel');
+insert into MOCK_DATA (id, fullname, email, password) values (8, 'Juliane', 'jwilleman7@domainmarket.com', 'Black-capped chickadee');
+insert into MOCK_DATA (id, fullname, email, password) values (9, 'Nellie', 'nfrodsam8@alexa.com', 'Owl, burrowing');
+insert into MOCK_DATA (id, fullname, email, password) values (10, 'Pebrook', 'pgamil9@sbwire.com', 'Miner''s cat');
